16c0ddaa36307ef1c58fa6134f6b55047346b535
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-02-19 09:27:09 +0200

    добавялет запрос на сервер и дебаунц инпута

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+const debounce = require('lodash.debounce');
+
+const inputAreaRef = document.querySelector('#input-area');
+
+inputAreaRef.addEventListener(
+  'input',
+  debounce(() => {
+    searchCountry(inputAreaRef.value);
+  }, 500),
+);
+
+function searchCountry(name) {
+  fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+    .then(res => res.json())
+    .then(country => console.log(country[0]));
+}
